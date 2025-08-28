97ad11f93f1ab1f80071b0cc38f1f848847181ec
gopalchavan45 <chavangopal811@gmail.com>
2025-08-28 21:40:26 +0530

    28-08-2025

--- learnings.js
+++ learnings.js
@@ -40,18 +40,71 @@ const p2 = new Promise((resolve, reject) => {
 // asyncData();
 //Note javascript first creation phase in that we are call function in that we have two promised that can be register and after creation phase it will move forward for execution ,it will put inside the callstack it will print first line of code and as soon as the print come the promised but still promised do not resolve in that case our js engine not await for that it suspended the function it will not block our single thread stack
 //2.after promised resolve once again function is put into stack start printing where is left
-async function asyncData() {
-  // js engine wait for the promised to be resolve
-  const respones = await p;
-  console.log("i will print after the promised resolve");
-  console.log(respones);
+// async function asyncData() {
+//   // js engine wait for the promised to be resolve
+//   const respones = await p;
+//   console.log("i will print after the promised resolve");
+//   console.log(respones);
 
-  const responesz = await p2;
-  console.log("i will print after the promised resolve");
-  console.log(responesz);
+//   const responesz = await p2;
+//   console.log("i will print after the promised resolve");
+//   console.log(responesz);
+// }
+// asyncData();
+// function g() {
+//   console.log("im single thread language");
+// }
+// g();
+
+//Callback
+// function getData(dataid, callback) {
+//   setTimeout(() => {
+//     console.log("data is here :" + dataid);
+//     if (callback) {
+//       console.log("form here call back function call..");
+//       callback();
+//     }
+//   }, 2000);
+// }
+// getData(1, () => {
+//   console.log("getting data2...");
+//   getData(2, () => {
+//     console.log("getting data3...");
+//     getData(3, () => {
+//       console.log("getting data4...");
+//       getData(4, () => {
+//         console.log("getting data5...");
+//         getData(5, () => {
+//           console.log("getting data6...");
+//           getData(0, () => {
+//             console.log("end");
+//           });
+//         });
+//       });
+//     });
+//   });
+// });
+
+let cart = ["gopal", "chavan"];
+createCard(cart)
+  .then((ele) => console.log(ele))
+  .catch((err) => {
+    console.log(err.message);
+  });
+function createCard(cart) {
+  return new Promise((resolve, reject) => {
+    if (!valided(cart)) {
+      const err = new Error("this is not valid");
+      reject(err);
+    }
+    let orderId = "1234";
+    if (orderId) {
+      setTimeout(() => {
+        resolve(orderId);
+      }, 5000);
+    }
+  });
 }
-asyncData();
-function g() {
-  console.log("im single thread language");
+function valided(cart) {
+  return false;
 }
-g();
