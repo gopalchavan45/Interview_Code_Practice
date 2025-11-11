4c0253ee37d88cb62b5f86a2d4a7a9c8cd767a2f
gopalchavan45 <chavangopal811@gmail.com>
2025-11-11 05:56:53 +0530

    11/11/2025

--- Sarthi/MapFilter.js
+++ Sarthi/MapFilter.js
@@ -39,3 +39,46 @@
 //6.polyfill for reduce function?
 //7.difference between map and filter?
 //-->map return some kind of value that value create new array and foreach method will not return anything as well as it will create new array it will change or modify the original array
+
+//output Based Questions
+
+//Que 1 : return only names of students in capital
+const students = [
+  { name: "Gopal", rollNum: 34, marks: 95 },
+  { name: "Raj", rollNum: 15, marks: 65 },
+  { name: "Madhav", rollNum: 50, marks: 77 },
+  { name: "Kartik", rollNum: 30, marks: 50 },
+];
+
+// const output = students.map((stu) => stu.name.toUpperCase());
+// console.log(output);
+
+//Que : return only details of that student who score greater than 60
+// const output1= students.filter((stu)=>stu.marks<60);
+// console.log(output1)
+
+//Que 3 : return whose marks greather than 60 and roll num grether tha 15
+
+// const output = students.filter((stu) => stu.marks > 60 && stu.rollNum > 15);
+// console.log(output)
+
+//Que 4:sum of all students marks
+
+// const output = students.reduce((acc,curr)=>acc+curr.marks,0)
+// console.log(output)
+
+//Que 5:return only name of student who score more less than 60
+
+// const output = students.filter((stu) => stu.marks < 60).map((stu) => stu.name);
+// console.log(output)
+
+//Que 6:return total marks of student with marks greater than 60 after 20 marks have been added to those scored less than 60
+
+// const output = students
+//   .map((stu) => {
+//     if (stu.marks < 60) {
+//       stu.marks += 20;
+//     }
+//     return stu;
+//   }).filter((stu) => stu.marks > 60).reduce((acc,cc)=>acc+cc.marks,0);
+//   console.log(output)
